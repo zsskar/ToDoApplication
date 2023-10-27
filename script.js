cd6f0672ac1605f157582b7f69614fdec1347568
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-10-27 23:24:38 +0530

    changes in design of list of items

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let add = document.getElementById("add");
-let oList = document.getElementById("list");
+let oList = document.getElementsByClassName("todo")[0];
 let list = [];
 add.addEventListener("click", function() {
     let text = document.getElementById("searchText").value;
     if(text){
         list.push(text);
-        let listItem = document.createElement('li');
-        listItem.innerText = text; 
-        oList.appendChild(listItem);
-        let remove = document.createElement('button');
-        remove.textContent = 'remove';
-        listItem.appendChild(remove)
-        remove.addEventListener('click',function(){
-            let parent  = remove.parentElement;
-            let index = list.indexOf(parent.innerHTML);
-            if (index > -1) { 
-                array.splice(index, 1);
-              }
-              oList.removeChild(parent);
+        oList.innerHTML += `  <li>${text}
+        <div class="buttons">
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </li>`;
+        // let listItem = document.createElement('li');
+        // listItem.innerText = text; 
+        // oList.appendChild(listItem);
+        // let remove = document.createElement('button');
+        // remove.textContent = 'remove';
+        // listItem.appendChild(remove)
+        // remove.addEventListener('click',function(){
+        //     let parent  = remove.parentElement;
+        //     let index = list.indexOf(parent.innerHTML);
+        //     if (index > -1) { 
+        //         array.splice(index, 1);
+        //       }
+        //       oList.removeChild(parent);
 
-        })
+        // })
     }
     else{
         alert("Enter the text !");
